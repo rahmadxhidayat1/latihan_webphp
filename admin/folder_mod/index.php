e78a15a48bf716dd24ad7bfd2a2cfeaf7905a3dc
rahmadxhidayat1 <sirlock1812@gmail.com>
2022-03-30 12:10:22 +0700

    Main

--- admin/folder_mod/index.php
+++ admin/folder_mod/index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 </head>
 <body>
     <!-- Button trigger modal -->
